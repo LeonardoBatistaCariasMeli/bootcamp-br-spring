76c7de76101b60e063e3feb5e6c11c1cf13f40de
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-05-26 11:04:53 -0300

    First commit

---
+++ src/test/java/br/com/digitalhouse/bootcampbrspring/BootcampBrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bootcampbrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampBrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
