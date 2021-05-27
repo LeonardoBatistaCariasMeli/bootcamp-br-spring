e15de291c5c8cd59dd7869106be1fa05db3f230e
Nycolas De Lima Vieira <nyvieira@mbpdemeliadmin.lan>
2021-05-27 15:14:20 -0300

    response entity

--- src/main/java/br/com/digitalhouse/bootcampbrspring/entrypoint/controller/CalculatorController.java
+++ src/main/java/br/com/digitalhouse/bootcampbrspring/entrypoint/controller/CalculatorController.java
@@ -2,7 +2,10 @@ package br.com.digitalhouse.bootcampbrspring.entrypoint.controller;
 
 import br.com.digitalhouse.bootcampbrspring.domain.entity.House;
 import br.com.digitalhouse.bootcampbrspring.usecase.CalculatorUseCase;
+import br.com.digitalhouse.bootcampbrspring.usecase.exceptions.DataIntegrityException;
 import br.com.digitalhouse.bootcampbrspring.usecase.model.HouseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -18,17 +21,35 @@ public class CalculatorController {
     }
 
     @GetMapping("/{year}/{month}/{day}")
-    public String ageCalculator(@PathVariable int year, @PathVariable int month, @PathVariable int day) {
-        LocalDate birthdate = LocalDate.of(year, month, day);
+    public ResponseEntity<String> ageCalculator(@PathVariable int year, @PathVariable int month, @PathVariable int day) {
+        try {
+            LocalDate birthdate = LocalDate.of(year, month, day);
 
-        var age = calculatorUseCase.calculateAge(birthdate);
+            var age = calculatorUseCase.calculateAge(birthdate);
 
-        return age == 0 ? "É UM BEBÊ" : "IDADE: " + age + " anos";
+            var response = age == 0 ? "É UM BEBÊ" : "IDADE: " + age + " anos";
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException ex) {
+
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/house")
-    public HouseResponse squareCalculator(@RequestBody House house) {
+    public ResponseEntity<Object> squareCalculator(@RequestBody House house) {
+        try {
 
-        return this.calculatorUseCase.calculateSquareMeters(house);
+            return new ResponseEntity<>(this.calculatorUseCase.calculateSquareMeters(house), HttpStatus.OK);
+        } catch (RuntimeException ex) {
+
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
