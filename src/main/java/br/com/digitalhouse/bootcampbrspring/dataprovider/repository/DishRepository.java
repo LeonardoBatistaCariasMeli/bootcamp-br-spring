393b3427a500a1a0843bd2dbfabd678fa7a02c78
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-05-31 09:16:07 -0300

    remocao da parte referenciada a api de restaurant

--- src/main/java/br/com/digitalhouse/bootcampbrspring/dataprovider/repository/DishRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.digitalhouse.bootcampbrspring.dataprovider.repository;
-
-import br.com.digitalhouse.bootcampbrspring.dataprovider.repository.entity.DishData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DishRepository extends JpaRepository<DishData, Integer> {
-}
--- src/main/java/br/com/digitalhouse/bootcampbrspring/domain/gateways/DishGateway.java
+++
@@ -1,8 +0,0 @@
-package br.com.digitalhouse.bootcampbrspring.domain.gateways;
-
-import br.com.digitalhouse.bootcampbrspring.domain.entity.Dish;
-
-public interface DishGateway {
-
-    public Dish findById(Integer dishId);
-}
